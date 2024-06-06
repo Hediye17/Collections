37cba57b0b8d587a22d2324261ef5d639fd8acb3
Hediye Kartal <88762730+Hediye17@users.noreply.github.com>
2024-06-06 20:34:52 +0300

    Add files via upload

---
+++ SetSample.java
@@ -0,0 +1,61 @@
+package Collections;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetSample {
+    public static void HashSet(){
+        Set<String> days=new HashSet<>();
+        days.add("Sunday");
+        days.add("Monday");
+        days.add("Saturday");
+        days.add("Monday");
+        days.add("monday");
+        days.add("Wednesday");
+        for (String day:days){
+            System.out.println(day);
+        }
+        //Tekrarlanan günleri terminal ekranına yazmaz.
+        //yukarıda yazıldığı şekilde değil karışık listeler
+        System.out.println("Toplam gün sayısı: "+days.size());
+    }
+
+    public static void LinkedHashSet(){
+        Set<String> members=new LinkedHashSet<>();
+        members.add("Murat");
+        members.add("Berk");
+        members.add("Elif");
+        members.add("Berk");
+        members.add("berk");
+        members.add("Canan");
+        System.out.println(" Üye Listesi\n");
+        //Tekrarlanan günleri terminal ekranına yazmaz.
+        //Yazıldığı şekilde listeler
+        for (String member:members){
+            System.out.println(member);
+        }
+        System.out.println("Üye sayısı: "+members.size());
+    }
+    public static void Treeset(){
+        Set<String> cityName=new TreeSet<>();
+        cityName.add("Barcelona");
+        cityName.add("Venice");
+        cityName.add("Roma");
+        cityName.add("Paris");
+        cityName.add("Venice");
+        cityName.add("Canakkale");
+        //Tekrarlanan Şehirleri terminal ekranına yazmaz.
+        //Yazıldığı şekilde değil alfabetik sırayla terminale listeler
+        for (String city:cityName){
+            System.out.println(city);}
+        System.out.println("Şehir sayısı:"+cityName.size());
+
+    }
+    public static void main(String[] args) {
+        HashSet();
+        LinkedHashSet();
+        Treeset();
+    }
+}
